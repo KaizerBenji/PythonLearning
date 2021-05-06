c3af48af3672fe127ccbd70f55989b62bfc01f20
Benjamin Gray <benjamin.gray3@yahoo.com>
2021-05-06 14:32:13 -0400

    update

---
+++ My Python Learning/Object Oriented Programming - Python/Rectangle Area.py
@@ -0,0 +1,5 @@
+#Find Rectangle Area
+L = int(input("Enter Length: ")) #Enter length of the rectangle
+W = int(input("Enter Width: ")) #Enter width of the rectangle
+area = W * L #calculate the area of the rectangle
+print(area) #display the results of the calculation
