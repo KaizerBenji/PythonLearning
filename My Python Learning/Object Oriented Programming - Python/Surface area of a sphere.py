99d1ee009c83849177db0262be1d596e097215c4
Benjamin Gray <benjamin.gray3@yahoo.com>
2021-05-24 12:46:52 -0400

    chapter 3 class work

---
+++ My Python Learning/Object Oriented Programming - Python/Surface area of a sphere.py
@@ -0,0 +1,5 @@
+#Surface area of a sphere
+import math
+radius = float(input("What is the radius of the sphere: "))
+surfArea = 4 * math.pi * radius**2
+print("The surface area of the sphere is: %.2f" %surfArea)
---
+++ My Python Learning/Object Oriented Programming - Python/Volume of a Sphere.py
@@ -0,0 +1,5 @@
+#Volume of a Sphere
+import math
+radius = float(input("What is the radius of the sphere: "))
+volume = (4/3) * math.pi * radius**3
+print("\n The volume of the sphere is =%.2f" %volume)
