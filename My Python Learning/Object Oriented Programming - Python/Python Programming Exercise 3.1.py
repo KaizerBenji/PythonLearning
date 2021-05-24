65b41144380cb6aa34581537551d157880a8f7af
Benjamin Gray <benjamin.gray3@yahoo.com>
2021-05-24 12:46:39 -0400

    chapter 3 class work

--- My Python Learning/Object Oriented Programming - Python/Python Programming Exercise 3.1.py
+++ My Python Learning/Object Oriented Programming - Python/Python Programming Exercise 3.1.py
@@ -1,7 +1,7 @@
 a = int(input("Input side a: "))
 b = int(input("Input side b: "))
 c = int(input("Input side c: "))
-if (a==b) and (a==c):
+if (a == b) and (a == c):
     print("The triangle is equilateral.")
 else:
     print("The triangle is not equilateral.")
