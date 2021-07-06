24b36022da69d63426f69625156a943492eedc64
Benjamin Gray <benjamin.gray3@yahoo.com>
2021-07-06 10:53:45 -0400

    Project 1/100

---
+++ My Python Learning/The 100 Projects/Band Name Generator.py
@@ -0,0 +1,10 @@
+# Project 1: Band Name Generator
+
+# Greeting.
+print("This is the Band Name Generator!")
+# User enters city name.
+city = input("What city did you grow up in?\n")
+# User enters pet name.
+pet = input("What is the name of you pet?\n")
+# User city and pet name are combined.
+print("Here is a suggestion for your band name: " + city + " " + pet)
